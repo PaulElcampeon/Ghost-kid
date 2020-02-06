b48145da13e1a817eb54d48db6e42cc9029362c9
PaulElcampeon <pauloladele@live.co.uk>
2020-02-06 17:35:08 +0000

    Added colliders to doors

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    public LawnmowerMission lawnmower;
+    public Renderer longGrassSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        longGrassSprite = GetComponent<Renderer>();
+        lawnmower = GameObject.FindObjectOfType<LawnmowerMission>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "LawnMower" && lawnmower.isGassed)
+        {
+            Debug.Log("Grass cut");
+            longGrassSprite.enabled = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
