bd7e26a75f3461fe7c5d98f725d2117dfb7d7fca
PaulElcampeon <pauloladele@live.co.uk>
2020-02-07 21:05:51 +0000

    BGM has been added for different floors now

---
+++ Assets/Scripts/Sound/PlayMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioSource[] bgm;
+    public float timeToFadeIn;
+    public int maxVolume;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Damnit");
+            SoundEngine.instance.UpdateMusic(bgm, timeToFadeIn, maxVolume);
+        } 
+    }
+}
---
+++ Assets/Scripts/Sound/SoundEngine.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEngine : MonoBehaviour
+{
+    public AudioSource[] bgm;
+    public float timeForMusicToFadeOut = 2f;
+
+    public static SoundEngine instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void PlaySFX(AudioSource sfx)
+    {
+        sfx.Play();
+    }
+
+    public void UpdateMusic(AudioSource[] musicTracks, float timeToFadeIn, int maxVolume)
+    {
+        StopMusic();//Stop music that is already playing
+        PlayMusic(musicTracks, timeToFadeIn, maxVolume);//Start to play new music
+    }
+
+
+    public void PlayMusic(AudioSource[] musicTracks, float timeToFadeIn, int maxVolume)
+    {
+        foreach(AudioSource track in musicTracks)
+        {
+            StartCoroutine(FadeIn(track, timeToFadeIn, maxVolume));
+        }
+    }
+
+    public void StopMusic()
+    {
+        foreach(AudioSource musicTrack in bgm)
+        {
+            if(musicTrack.isPlaying)
+            {
+                StartCoroutine(FadeOut(musicTrack, timeForMusicToFadeOut));
+            }
+        }
+    }
+
+    public IEnumerator FadeIn(AudioSource audioSource, float FadeTime, int musicVolume)
+    {
+        audioSource.volume = 0;
+        audioSource.Play();
+        while (audioSource.volume < musicVolume)
+        {
+            audioSource.volume += Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+}
